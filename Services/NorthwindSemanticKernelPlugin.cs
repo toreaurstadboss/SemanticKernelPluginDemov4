56b4213d137b56b2c23eefb47dd6f5a14fd44ba8
Tore Aurstad <tore.aurstad@gmail.com>
2025-04-15 01:54:38 +0200

    Tidy up layout

--- Services/NorthwindSemanticKernelPlugin.cs
+++ Services/NorthwindSemanticKernelPlugin.cs
@@ -25,5 +25,21 @@ namespace SemanticKernelPluginDemov4.Services
             }
         }
 
+        [KernelFunction]
+        [Description("When asked about the total sales of a given month in a year, use this method. In case asked for multiple months, call this method again multiple times, adjusting the month and year as provided. The month and year is to be in the range 1-12 for months and for year 1996-1998. Suggest for the user what the valid ranges are in case other values are provided.")]
+        public async Task<decimal> GetTotalSalesInMontAndYear(int month, int year)
+        {
+            using (var dbContext = _northwindContext.CreateDbContext())
+            {
+                var sumOfOrders = await (from Order o in dbContext.Orders
+                             join OrderDetail od in dbContext.OrderDetails on o.OrderId equals od.OrderId
+                             where o.OrderDate.HasValue && (o.OrderDate.Value.Month == month
+                             && o.OrderDate.Value.Year == year) 
+                             select (od.UnitPrice * od.Quantity) * (1 - (decimal)od.Discount)).SumAsync();
+
+                return sumOfOrders;
+            }
+        }
+
     }
 }
